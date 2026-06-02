a7a6a79b97a1d87a914dba68cc01e812e1a7fecf
Your Name <you@example.com>
2026-06-02 14:58:38 +0400

    Add task4

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -10,6 +10,30 @@ interface Teacher {
 interface Directors extends Teacher{
     numberOfReports: number;
 }
+
 function printTeacher(firstName: string, lastName:string): string {
    return `${firstName[0]}. ${lastName}`;
 }
+
+interface StudentClassInterface{
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface StudentClassConstructor{
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+    return this.firstName;
+}}
