d74f03b0b0194f06d11826e1be811cbdf9032728
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-11 18:04:31 +0400

    Add task2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+
+  entry: './js/dashboard_main.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'javascript/auto',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              esModule: false,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  performance: {
+    hints: false,
+  },
+};
