4c3111fdb41b2fe199668bdaec6ef01143eaf941
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-13 16:11:51 +0400

    Add task1

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -2,35 +2,36 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App Component Tests', () => {
-
-  // Test 1: Verify the Heading element
   test('renders the h1 element with School Dashboard text', () => {
     render(<App />);
-    const headingElement = screen.getByRole('heading', { name: /school dashboard/i, level: 1 });
+
+    const headingElement = screen.getByRole('heading', {
+      name: /school dashboard/i,
+      level: 1,
+    });
+
     expect(headingElement).toBeInTheDocument();
   });
 
-  // Test 2: Verify the paragraph texts in body and footer
   test('renders correct text content in the body and footer paragraphs', () => {
     render(<App />);
 
-    // Check body paragraph content
-    const bodyParagraph = screen.getByText(/login to access the full dashboard/i);
+    const bodyParagraph = screen.getByText(
+      /login to access the full dashboard/i
+    );
+
+    const footerRegex = /copyright \d{4}.*holberton school/i;
+    const footerNode = screen.getByText(footerRegex);
+
     expect(bodyParagraph).toBeInTheDocument();
-
-    // Check footer paragraph content
-    const currentYear = new Date().getFullYear();
-
-    const footerParagraph = screen.getByText(/copyright \d{4}.*holberton school/i);
-
-    expect(footerParagraph).toBeInTheDocument();
-    });
-
-  // Test 3: Verify the Holberton logo image element
-  test('renders the logo image element with correct alt text', () => {
-    render(<App />);
-    const logoImage = screen.getByRole('img', { name: /holberton logo/i });
-    expect(logoImage).toBeInTheDocument();
+    expect(footerNode).toBeInTheDocument();
   });
 
+  test('renders the logo image element with correct alt text', () => {
+    render(<App />);
+
+    const logoImage = screen.getByAltText(/holberton logo/i);
+
+    expect(logoImage).toBeInTheDocument();
+  });
 });
