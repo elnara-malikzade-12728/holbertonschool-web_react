4b2bd8605dedc1af7491613dce7c744dde4aa8c3
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-16 19:25:56 +0400

    update App for task3

--- react_component/task_3/dashboard/src/App/App.jsx
+++ react_component/task_3/dashboard/src/App/App.jsx
@@ -6,6 +6,8 @@ import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 
 class App extends Component {
   static defaultProps = {
@@ -74,11 +76,18 @@ class App extends Component {
           <Header />
 
           {isLoggedIn ? (
-            <CourseList courses={coursesList} />
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList courses={coursesList} />
+            </BodySectionWithMarginBottom>
           ) : (
-            <Login />
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
           )}
 
+          <BodySection title="News from the School">
+            <p>Holberton School news goes here</p>
+          </BodySection>
           <Footer />
         </div>
       </Fragment>
