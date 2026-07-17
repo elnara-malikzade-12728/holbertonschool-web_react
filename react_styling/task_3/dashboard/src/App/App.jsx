bf94c7d94072951f392817d660d135e50070b8eb
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-17 15:58:39 +0400

    Add task_3

--- react_styling/task_3/dashboard/src/App/App.jsx
+++ react_styling/task_3/dashboard/src/App/App.jsx
@@ -1,5 +1,4 @@
 import { Component, Fragment } from 'react';
-import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
