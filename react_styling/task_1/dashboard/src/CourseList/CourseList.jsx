bcc12adedef72adf0bd09e249e77d929aa4947b2
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-17 11:33:36 +0400

    Add task1

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -2,46 +2,42 @@ import CourseListRow from './CourseListRow';
 import WithLogging from '../HOC/WithLogging';
 
 function CourseList({ courses = [] }) {
-  if (courses.length === 0) {
-    return (
-      <div className="w-[80%] mx-auto my-8 overflow-x-auto">
-        <table id="CourseList" className="w-full">
-          <thead>
+  return (
+    <div className="mx-auto my-8 w-[80%] overflow-x-auto">
+      <table id="CourseList" className="w-full">
+        <thead>
+          {courses.length === 0 ? (
             <CourseListRow
               isHeader
               textFirstCell="No course available yet"
             />
-          </thead>
-        </table>
-      </div>
-    );
-  }
+          ) : (
+            <>
+              <CourseListRow
+                isHeader
+                textFirstCell="Available courses"
+              />
 
-  return (
-    <div className="w-[80%] mx-auto my-8 overflow-x-auto">
-      <table id="CourseList" className="w-full">
-        <thead>
-          <CourseListRow
-            isHeader
-            textFirstCell="Available courses"
-          />
-
-          <CourseListRow
-            isHeader
-            textFirstCell="Course name"
-            textSecondCell="Credit"
-          />
+              <CourseListRow
+                isHeader
+                textFirstCell="Course name"
+                textSecondCell="Credit"
+              />
+            </>
+          )}
         </thead>
 
-        <tbody>
-          {courses.map((course) => (
-            <CourseListRow
-              key={course.id}
-              textFirstCell={course.name}
-              textSecondCell={course.credit}
-            />
-          ))}
-        </tbody>
+        {courses.length > 0 && (
+          <tbody>
+            {courses.map((course) => (
+              <CourseListRow
+                key={course.id}
+                textFirstCell={course.name}
+                textSecondCell={course.credit}
+              />
+            ))}
+          </tbody>
+        )}
       </table>
     </div>
   );
