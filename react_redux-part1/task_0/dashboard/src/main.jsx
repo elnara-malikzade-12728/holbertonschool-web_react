e6977729d9e56275a8cedd774f659a9c85333173
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-25 09:59:36 +0400

    Fix task0

--- react_redux-part1/task_0/dashboard/src/main.jsx
+++ react_redux-part1/task_0/dashboard/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import './main.css'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
