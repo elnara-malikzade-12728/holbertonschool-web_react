3e8741da1807591a2940d3febc34b6fb15f8b96b
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-25 20:14:17 +0400

    Add task6

---
+++ react_redux-part1/task_3/dashboard/src/app/rootReducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+import authReducer from '../features/auth/authSlice.js';
+import notificationsReducer from '../features/notifications/notificationsSlice.js';
+import coursesReducer from '../features/courses/coursesSlice.js';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  notifications: notificationsReducer,
+  courses: coursesReducer,
+});
+
+export default rootReducer;
---
+++ react_redux-part1/task_3/dashboard/src/features/auth/authSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const initialState = {
+  user: {
+    email: '',
+    password: '',
+  },
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+
+  reducers: {
+    login: (state, action) => {
+      state.user.email = action.payload.email;
+      state.user.password =
+        action.payload.password;
+      state.isLoggedIn = true;
+    },
+
+    logout: (state) => {
+      state.user.email = '';
+      state.user.password = '';
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const {
+  login,
+  logout,
+} = authSlice.actions;
+
+export default authSlice.reducer;
