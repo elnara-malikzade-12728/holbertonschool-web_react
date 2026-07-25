2e5e69f42af1210755ca5aa641838f832b54859f
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-25 15:49:31 +0400

    Fix task4

--- react_hooks/task_4/dashboard/src/Header/Header.jsx
+++ react_hooks/task_4/dashboard/src/Header/Header.jsx
@@ -11,33 +11,43 @@ function Header() {
   };
 
   return (
-    <>
-      <header
+    <header
+      className="
+        flex
+        flex-col
+        items-center
+        justify-center
+        min-[912px]:flex-row
+        min-[912px]:justify-start
+        min-[912px]:px-5
+      "
+    >
+      <img
+        src={holbertonLogo}
+        alt="holberton logo"
+        className="
+          -mt-10
+          h-100
+          w-100
+          object-contain
+          min-[520px]:h-80
+          min-[520px]:w-80
+          min-[912px]:h-40
+          min-[912px]:w-40
+        "
+      />
+
+      <div
         className="
           flex
           flex-col
           items-center
-          justify-center
+          min-[912px]:ml-5
           min-[912px]:flex-row
-          min-[912px]:justify-start
-          min-[912px]:px-5
+          min-[912px]:items-center
+          min-[912px]:gap-3
         "
       >
-        <img
-          src={holbertonLogo}
-          alt="holberton logo"
-          className="
-            -mt-10
-            h-100
-            w-100
-            object-contain
-            min-[520px]:h-80
-            min-[520px]:w-80
-            min-[912px]:h-40
-            min-[912px]:w-40
-          "
-        />
-
         <h1
           className="
             -mt-20
@@ -46,38 +56,40 @@ function Header() {
             font-bold
             text-main
             min-[520px]:text-3xl
-            min-[912px]:ml-5
             min-[912px]:mt-0
             min-[912px]:text-4xl
           "
         >
           School Dashboard
         </h1>
-      </header>
 
-      {user.isLoggedIn && (
-        <div
-          id="logoutSection"
-          className="
-            px-3
-            pb-2
-            text-center
-            text-xs
-            min-[912px]:px-5
-            min-[912px]:text-left
-          "
-        >
-          Welcome {user.email}{' '}
-          <a
-            href="#logout"
-            onClick={handleLogout}
-            className="cursor-pointer underline"
+        {user.isLoggedIn && (
+          <div
+            id="logoutSection"
+            className="
+              whitespace-nowrap
+              text-xs
+              min-[912px]:text-sm
+            "
           >
-            (logout)
-          </a>
-        </div>
-      )}
-    </>
+            Welcome <strong>{user.email}</strong>{' '}
+            <a
+              href="#logout"
+              onClick={handleLogout}
+              className="
+                cursor-pointer
+                text-purple-700
+                hover:text-red-600
+                active:text-red-600
+                transition-colors
+              "
+            >
+              (logout)
+            </a>
+          </div>
+        )}
+      </div>
+    </header>
   );
 }
 
